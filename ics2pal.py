7e0e6bb7b4ccf978850f251fb1c600a7e4201faa
Matthew Via <via@matthewvia.info>
2012-05-24 17:21:07 -0400

    Initial commit

---
+++ ics2pal.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+import email.parser
+import sys
+import vobject
+import datetime
+
+
+if __name__ == "__main__":
+
+
+  #read rfc822 message from stdin
+  parser = email.parser.Parser()
+  msg = parser.parse(sys.stdin)
+
+
+  if not msg.is_multipart():
+    print "Not a multipart MIME message!"
+
+  for part in msg.get_payload():
+    if part.get_content_type() == "application/ics":
+      event = part.get_payload(decode=True)
+      cal = vobject.readOne(event)
+
+      summary = cal.vevent.summary.valueRepr()
+      stdate = cal.vevent.dtstart.value
+      organizer = cal.vevent.organizer.valueRepr()
+      if organizer.startswith("MAILTO:"):
+        organizer = organizer[7:]
+
+      paltime = stdate.strftime("%Y%d%m")
+
+      palline = "%s    %s (from %s)" % (paltime, summary, organizer)
+
+      print palline
+
